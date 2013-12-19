11e41f53d4b6de768b250458101d31a09fbafa0c
Filix <syp.syl@gmail.com>
2013-12-19 22:30:30 +0800

    up

---
+++ src/Dngu/WebBundle/Controller/SettingController.php
@@ -0,0 +1,47 @@
+<?php
+namespace Dngu\WebBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class SettingController extends BaseController
+{
+    /**
+     * 基本设置
+     * @Template()
+     */
+    public function basicAction(){
+        return array();
+    }
+    
+    /**
+     * 病历设置
+     * @Template()
+     */
+    public function caseAction(){
+        
+    }
+    
+    /**
+     * 个性设置
+     * @Template()
+     */
+    public function personalityAction(){
+        
+    }
+    
+    /**
+     * 密码设置
+     * @Template()
+     */
+    public function passportAction(){
+        
+    }
+    
+    /**
+     * 绑定设置
+     * @Template()
+     */
+    public function bindAction(){
+        
+    }
+}
